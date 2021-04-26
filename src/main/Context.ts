f3a7579f8859cb1def24fbc104d94f9624f81811
nnnnusui <nnnnusui@gmail.com>
2021-04-27 06:30:10 +0900

    add: Context init function

--- src/main/Context.ts
+++ src/main/Context.ts
@@ -1,6 +1,11 @@
 import { Char } from "./Char";
+import "./Char";
 
 export type Context = {
   src: Char[];
   offset: number;
 };
+export const init = (source: string): Context => ({
+  src: source.chars(),
+  offset: 0,
+});
