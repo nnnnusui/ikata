81d715e44de99fe9ef428af0a16e1842d4e3cd01
nnnnusui <nnnnusui@gmail.com>
2021-04-27 22:06:01 +0900

    add: transpile function

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
+import { chainR, option, or, repeat } from "combinator-node";
 import { init } from "./main/Context";
+import { interval } from "./main/parser/interval";
 import { defineFunction } from "./main/tokenizer/defineFunction";
+import { rawLiteral } from "./main/tokenizer/rawLiteral";
+import { transpile } from "./main/transpiler/transpile";
 
-console.dir(defineFunction(init(`f main: void = {raw("sample")} `)), {
-  depth: Number.MAX_VALUE,
-});
+const topLevel = or(defineFunction, rawLiteral);
+const syntax = repeat(chainR(option(interval), topLevel));
+const parse = (source: string) => syntax(init(source));
+
+const text = `
+raw("
+|target triple = "x86_64-unknown-unknown-elf"
+")
+f KernelMain: void = {
+  raw("
+  |  br label %1
+  |1:
+  |  call void asm sideeffect "hlt", ""()
+  |  br label %1
+  ")
+}
+`;
+const result = parse(text);
+console.dir(result, { depth: Number.MAX_VALUE });
+if (!result.ok) throw new Error("");
+
+const transpiled = result.get.map((it) => transpile(it)).join("");
+console.log(transpiled);
---
+++ src/main/transpiler/transpile.ts
@@ -0,0 +1,16 @@
+import { Token, TokenKind } from "../Token";
+
+export const transpile = (it: Token<TokenKind>): string => {
+  switch (it.kind) {
+    case "define function": {
+      const { name, returnType, function: fun } = it.value;
+      return `define ${returnType} ${name} ${transpile(fun)}`;
+    }
+    case "function literal":
+      return `{\n${it.value.orders.map((it) => transpile(it)).join("")}}`;
+    case "raw literal":
+      return `${it.value.value}\n`;
+    default:
+      return "";
+  }
+};
