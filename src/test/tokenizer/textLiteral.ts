36c0d8878f3595e1320df22f1bd63423755a5f78
nnnnusui <nnnnusui@gmail.com>
2021-04-27 12:25:40 +0900

    update: test textLiteral use tokenizerTesterFrom

--- src/test/tokenizer/textLiteral.ts
+++ src/test/tokenizer/textLiteral.ts
@@ -1,14 +1,9 @@
-import { expect } from "chai";
-import { init } from "../../main/Context";
 import { textLiteral } from "../../main/tokenizer/textLiteral";
-import "../env";
+import { tokenizerTesterFrom } from "../env";
 
-const ok = (value: string) => ({
-  ok: true,
-  get: { kind: "text literal", value },
-});
-const check = (source: string) => expect(textLiteral(init(source))).to;
-describe("textLiteral", () => {
+const kind = "text literal";
+const { ok, check } = tokenizerTesterFrom(kind, textLiteral);
+describe(kind, () => {
   describe("fail with", () => {
     it("enclosure not exists", () => check(`tes`).containSubset({ ok: false }));
     it("enclosure don't started", () =>
@@ -21,10 +16,8 @@ describe("textLiteral", () => {
     it("empty", () => check(`""`).containSubset(ok("")));
 
     it("one line", () =>
-      expect(
-        check(`"one line\ttext literal"`).containSubset(
-          ok("one line\ttext literal")
-        )
+      check(`"one line\ttext literal"`).containSubset(
+        ok("one line\ttext literal")
       ));
 
     it("paling by pipe", () =>
