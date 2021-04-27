3dc1c442426e3cc68c52f170cab8a03b9cb27711
nnnnusui <nnnnusui@gmail.com>
2021-04-27 23:57:10 +0900

    fix: `define function` transpiler

--- src/main/transpiler/transpile.ts
+++ src/main/transpiler/transpile.ts
@@ -6,7 +6,7 @@ export const transpile = (it: Token<TokenKind>): string => {
       return it.value.map((it) => transpile(it)).join("");
     case "define function": {
       const { name, returnType, function: fun } = it.value;
-      return `define @${returnType} ${name} ${transpile(fun)}`;
+      return `define ${returnType} @${name}() ${transpile(fun)}`;
     }
     case "function literal":
       return `{\n${it.value.orders.map((it) => transpile(it)).join("")}}`;
